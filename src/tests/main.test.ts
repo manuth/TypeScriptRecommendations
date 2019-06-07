136f6ddc210d60fe19e53b8ebf7313d5895f05be
Manuel Thalmann <m@nuth.ch>
2019-06-07 19:40:09 +0200

    Initialize a project  for testing the settings

---
+++ src/tests/main.test.ts
@@ -0,0 +1,13 @@
+import * as assert from "assert";
+
+suite(
+    "TypeScriptRecommendations",
+    () =>
+    {
+        test(
+            "Example...",
+            () =>
+            {
+                assert.strictEqual(1, 1);
+            });
+    });
